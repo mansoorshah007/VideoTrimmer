2f095811d957391b3da153b65a085449017838d7
mansoorshah007 <mansoorshah1003@gmail.com>
2023-12-02 13:09:15 +0500

    Python scriptto trimm a video

---
+++ trim-video.py
@@ -0,0 +1,26 @@
+from moviepy.video.io.VideoFileClip import VideoFileClip
+import os
+
+def trimAndCompressVideo(inputPath, outputPath, startTime = 0, endTime = 10, bitrate = "1500k"):
+
+    _, inputExtension = os.path.splitext(inputPath)
+    videoClip = VideoFileClip(inputPath)
+    trimmedClip = videoClip.subclip(startTime, endTime)
+
+    outputFormat = "mp4"
+
+    _, outputExtension = os.path.splitext(outputPath)
+    if outputExtension.lower() != '.mp4':
+        outputPath = os.path.splitext(outputPath)[0] + ".mp4"
+
+    trimmedClip.write_videofile(outputPath, codec = "libx264", audio_codec = "aac", bitrate = bitrate)
+
+    videoClip.close()
+    trimmedClip.close()
+
+if __name__ == "__main__":
+    inputVideoPath  = "input-video.mp4"
+    outputVideoPath = "output-video.mp4"
+
+    trimAndCompressVideo(inputVideoPath, outputVideoPath)
+
